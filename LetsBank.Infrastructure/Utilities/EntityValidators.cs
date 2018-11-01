ca26b703e9629088254e0aba4383a5fda39ff696
Mohan Selvamoorthy <mohan.selvamoorthy@onsolve.com>
2018-11-01 09:54:48 -0400

    Add Entity Validation Helpers

---
+++ LetsBank.Infrastructure/Utilities/EntityValidators.cs
@@ -0,0 +1,28 @@
+﻿using LetsBank.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LetsBank.Infrastructure.Utilities
+{
+    public static class EntityValidators
+    {
+		public static bool UserIsValid(User user)
+		{
+			var context = new ValidationContext(user, serviceProvider: null, items: null);
+			var results = new List<ValidationResult>();
+
+			var isValid = Validator.TryValidateObject(user, context, results);
+
+			if (!isValid)
+			{
+				foreach (var validationResult in results)
+				{
+					Console.WriteLine(validationResult.ErrorMessage);
+				}
+			}
+
+			return isValid;
+		}
+	}
+}
